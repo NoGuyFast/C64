14b07063afe09d21794caa548a150f972fc5e32d
NoGuyFast <167024453+NoGuyFast@users.noreply.github.com>
2025-02-16 21:35:26 +0100

    Update 50FPS_VectorCube_RealtimeRotation.asm

--- VectorCube/50FPS_VectorCube_RealtimeRotation.asm
+++ VectorCube/50FPS_VectorCube_RealtimeRotation.asm
@@ -4,9 +4,9 @@
 ;*.https://codebase64.org/doku.php?id=magazines:chacking8
 
 
-;*. It took them 8 Days to make it - it took me 8 Days 
+;*. it took me 8 Days 
 ;*. to understand it and make my own Code out of that article 
-;*. a week later my code is gone from 23 FPS to 31 FPS
+;*. a week later my code is gone from 23 FPS to 31 FPS another week later I optimized everthing to get 50 FPS
 ;*. Additions-Rotations-Matrix .*
 ;*.  Code by CJoke 01/2022     .*
 
